d2b640f7b23079e9e7caba791f786af9c46f3037
Marina Alieva <120053456+mgitalieva@users.noreply.github.com>
2023-01-16 23:58:37 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+valid_email = "eeeeeee32423eewerewte@gmail.com"
+valid_password = "515521_Jck"
+invalid_key = {'key': 'aaaaaaaaaaaaaaaaaaaaaa1111111111111111111111111111111111'}
+
+big_name = 'ergregregergregregregregegrgerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrfffffffffffffffffffffffffegggfgfd'\
+           'fgfdddddddddffffffffffffffffffft546546564364333333333333333333333333333333333333333333333333333333333333tg'\
+           'regreggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggvtrgtrhthrhtrhgtrgtgg'\
+           'grrrrrrrrrrrrrrrrrrrrrrrrrrrrrrge45345435843054regregregrgrgreehrehregrrgggggggggggggggggggggggggggggggggg'
+
+symbol_type = '!@@#!#)())*(&()'
